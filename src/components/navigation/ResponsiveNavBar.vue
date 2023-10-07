ef0c0656e5ff202224162324ac8849efb9099350
nativ.sibony <nativ.sibony@aquasec.com>
2023-10-07 13:46:05 +0300

    .

--- src/components/navigation/ResponsiveNavBar.vue
+++ src/components/navigation/ResponsiveNavBar.vue
@@ -1,28 +1,31 @@
 <template>
-  <div class="py-10">
-    <Icon name="icon-menu" class="fill-dark-grayish-blue cursor-pointer mt-[0.2rem]" @click="open = true" />
-  </div>
-  <div
-    class="fixed top-0 left-0 bg-black bg-opacity-50 transition-colors duration-500 w-full h-full"
-    :class="{ 'opacity-100': open, 'opacity-0': !open }"
-  >
-    <div
-      class="absolute top-0 left-0 bg-white h-full flex flex-col overflow-hidden transition-[width] duration-500"
-      :class="{ 'w-[70%]': open, 'w-0': !open }"
-    >
-      <Icon
-        name="icon-close"
-        class="fill-dark-grayish-blue cursor-pointer transition-all duration-500 mt-10 ml-5"
-        :class="{ 'opacity-0': !open }"
-        width="20"
-        height="20"
-        @click="open = false"
-      />
-
-      <ul>
-        <li>1</li>
-      </ul>
+  <div class="transition-all duration-500 z-50">
+    <div class="py-10 px-4 cursor-pointer" @click="open = true">
+      <Icon name="icon-menu" class="fill-dark-grayish-blue cursor-pointer mt-[0.2rem]"  />
     </div>
+    <div
+      class="fixed top-0 left-0 bg-black bg-opacity-50 transition-all duration-500 w-full h-full"
+      :class="{ 'opacity-100': open, 'opacity-0 pointer-events-none': !open }"
+      @click.self="open = false"
+    >
+      <div
+        class="absolute top-0 left-0 bg-white h-full flex flex-col overflow-hidden transition-all duration-500 z-100"
+        :class="{ 'w-[70%]': open, 'w-0': !open }"
+      >
+        <Icon
+          name="icon-close"
+          class="fill-dark-grayish-blue cursor-pointer transition-all duration-500 mt-10 ml-8"
+          :class="{ 'opacity-0': !open }"
+          width="20"
+          height="20"
+          @click="open = false"
+        />
+  
+        <ul>
+          <li>1</li>
+        </ul>
+      </div>
+  </div>
   </div>
 </template>
 
