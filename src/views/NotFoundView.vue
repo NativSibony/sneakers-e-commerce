982e29deef9bdf4889e3788653926c4aa77f74db
nativsibony <nativsibony@gmail.com>
2023-10-07 16:56:06 +0300

    fu

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col mt-40 items-center justify-center gap-3">
+    <h1 class="font-bold text-orange text-3xl">404 Not Found</h1>
+    <p class="font-semibold">The page you are looking for does not exist.</p>
+    <RouterLink to="/" class="bg-orange rounded-xl px-4 py-2 text-center flex items-center text-white font-semibold mt-5">
+      Home
+    </RouterLink>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+/* Add some styles here */
+</style>
